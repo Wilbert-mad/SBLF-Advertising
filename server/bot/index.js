79aa24f8897751aba4640568dac17f89407d2941
angel <angelbecerrah548@gmail.com>
2020-09-03 13:45:56 -0500

    feat(events): Add events handler
    Events handler to keep events more organized

--- server/bot/index.js
+++ server/bot/index.js
@@ -1,4 +1,5 @@
 const { Client } = require('discord.js');
+const { readdir } = require('fs').promises;
 
 require('dotenv').config();
 const client = new Client({
@@ -8,14 +9,23 @@ const client = new Client({
 client.commands = new Map();
 client.aliases = new Map();
 
-client.on('ready', () => console.log(`${client.user.tag} Is Ready`));
-
 module.exports = {
   async init() {
     try {
-      client.login(process.env.TOKEN);
+      // events handler
+      const evtFiles = await readdir('./server/bot/events');
+      evtFiles.forEach(f => {
+        const evtName = f.split('.')[0];
+        console.log(`Loading Event: ${evtName}`);
+        const event = require(`../bot/events/${f}`);
+        client.on(evtName, event.bind(null, client));
+      });
+      console.log(`Loading a total of ${evtFiles.length} events.`);
+
+      // login the client
+      await client.login(process.env.TOKEN);
     } catch (err) {
-      console.error('Error connection to discord: ', err);
+      console.error('Error connecting: ', err);
     }  
   },
 };
